76e0a41850706cca216772416fc2daba11d3065d
kyrian <ckyrian@protonmail.com>
2022-01-03 20:41:47 +0100

    accept textarea input

--- components/Form/CustomInput.tsx
+++ components/Form/CustomInput.tsx
@@ -1,25 +1,35 @@
-import { FormControl, Input, FormErrorMessage, FormErrorIcon, FormLabel, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { FormControl, Input, Textarea, FormErrorMessage, FormErrorIcon, FormLabel, InputGroup, InputRightElement } from "@chakra-ui/react"
 
 type props = {
     errors: string[] | undefined
     label: string
     name: string
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-    inputType: React.HTMLInputTypeAttribute
+    // TODO change type of event so it accepts both input and textarea
+    handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+    inputType: React.HTMLInputTypeAttribute | 'textarea'
     isRequired: boolean
 }
 
-const FormInput = ({ isRequired, label, errors, name, handleChange, inputType }: props) => {
+export const FormInput = ({ isRequired, label, errors, name, handleChange, inputType }: props) => {
     return (
         <FormControl mb="4" isInvalid={errors && true} isRequired={isRequired}>
             <FormLabel>{label}</FormLabel>
             <InputGroup>
-                <Input
-                    name={name}
-                    errorBorderColor="red.500"
-                    onChange={handleChange}
-                    type={inputType}
-                />
+                {inputType == 'textarea' ? (
+                    <Textarea
+                        name={name}
+                        errorBorderColor='red.500'
+                        onChange={handleChange}
+                        resize='vertical'
+                    />
+                ) : (
+                    <Input
+                        name={name}
+                        errorBorderColor="red.500"
+                        onChange={handleChange}
+                        type={inputType}
+                    />
+                )}
                 <InputRightElement children={<FormErrorIcon color="red.400" />} />
             </InputGroup>
             {errors?.map(msg => (
